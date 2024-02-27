5ba77e4e88554df9c42da2542cd0dca396a63ea2
shahbaz4783 <shahbaz4783@gmail.com>
2024-02-27 13:34:10 +0530

    count occurrences challenge

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,8 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+	let count = 0;
+
+	str.split('').forEach((item) => item === char && count++);
+	return count;
+}
 
 module.exports = countOccurrences;
