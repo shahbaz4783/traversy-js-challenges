e1b019137fe88c7fde6583d6297745fb361e94ef
shahbaz4783 <shahbaz4783@gmail.com>
2024-02-27 13:43:03 +0530

    find max num  challenge

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,9 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+	let max = arr[0];
+	for (let i = 1; i < arr.length; i++) {
+		arr[i] > max && (max = arr[i]);
+	}
+	return max;
+}
 
 module.exports = findMaxNumber;
